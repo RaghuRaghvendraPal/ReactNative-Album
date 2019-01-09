d0275732b902a9e67426448a4bcbcc74611cedc3
RaghuRaghvendraPal <raghvendrapal65@gmail.com>
2019-01-09 18:56:29 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-/** @format */
+// Inport libraries
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+//  ./ starts from Current directory
+// Create Component -: A Component ia javascript function that returns some amount of JSX
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Header headerText={'Raghav'} />
+      <AlbumList />
+    </View>
+  );
+};
+// Both are same if i want
 
-AppRegistry.registerComponent(appName, () => App);
+// const App = () => (
+//     <Text>Raghvendra</Text>
+// );
+// Render component to screen-: ReactNative will do it
+AppRegistry.registerComponent('albums', () => App);
